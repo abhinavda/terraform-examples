4922ae79c3d638cb142411dfa268ace2a68f7921
abhinav <abhinavpitt@gmail.com>
2022-01-10 17:44:52 -0800

    using variables in different ways

---
+++ 2_using_variables/resource.tf
@@ -0,0 +1,77 @@
+// Create external static ip and associate it with an instance
+resource "google_compute_address" "static" {
+  name = "ipv4-address"
+}
+output "static_ip" {
+  value = google_compute_address.static.address
+}
+// End - external static ip and associate it with an instance
+
+// start - creating an instance
+data "google_compute_image" "debian_image" {
+  family  = "debian-9"
+  project = "debian-cloud"
+}
+resource "google_compute_instance" "instance_with_ip" {
+  name         = "vm-instance"
+  machine_type = var.machine_type_gcp
+  zone         = var.zone_given
+
+  boot_disk {
+    initialize_params {
+      image = data.google_compute_image.debian_image.self_link
+    }
+  }
+
+  network_interface {
+    network = "default"
+    access_config {
+      nat_ip = google_compute_address.static.address
+    }
+  }
+}
+// end - creating an instance
+
+// Creating a staging group
+resource "google_compute_instance_group" "staging_group" {
+  name      = "staging-instance-group"
+  zone      = var.zone_given
+  instances = [google_compute_instance.instance_with_ip.id]
+  named_port {
+    name = "http"
+    port = var.http_port
+  }
+
+  named_port {
+    name = "https"
+    port = var.https_port
+  }
+
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+// end - creation of staging group
+
+// start compute health
+resource "google_compute_https_health_check" "staging_health" {
+  name         = "staging-health"
+  request_path = "/health_check"
+}
+//end - compute health
+
+// start - staging backend service
+resource "google_compute_backend_service" "staging_service" {
+  name      = "staging-service"
+  port_name = "https"
+  protocol  = "HTTPS"
+
+  backend {
+    group = google_compute_instance_group.staging_group.id
+  }
+
+  health_checks = [
+    google_compute_https_health_check.staging_health.id,
+  ]
+}
+// end - staging backend service
---
+++ 2_using_variables/variables.tf
@@ -0,0 +1,27 @@
+variable "machine_type_gcp" {
+  default = "f1-micro"
+}
+
+variable "zone_given" {
+  default = "us-central1-a"
+}
+
+variable "region_given" {
+  default = "us-central1"
+}
+
+variable "http_port" {
+  default = "80"
+}
+
+variable "https_port" {
+  default = "443"
+}
+
+variable "svc_account" {
+  default = "/Users/abhinavdamarapati/projects/git/new_projects/terraform-examples/ardent-dream-337620-ac612f7b334e.json"
+}
+
+variable "project_name" {
+  default = "ardent-dream-337620"
+}
